15dc17946559c141c448267bdddd19951fd9f8a1
Gabriel Lucca Marsola <mgabriellucca@gmail.com>
2025-10-18 16:22:07 -0300

    Criei uma classe para revisar tipos + variáveis + operadores

---
+++ Fundamentos/TiposVariaveisEOperadores.java
@@ -0,0 +1,13 @@
+// 
+public class TiposVariaveisEOperadores {
+  public static void main(String[] args) {
+    int a = 7, b = 13;
+    int soma = a + b;
+    double media = soma / 2.0;
+    System.out.println("A soma é: " + soma);
+    System.out.println("A média é " + media);
+
+
+  }
+  
+}
