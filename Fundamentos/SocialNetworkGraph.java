bd5028ed505a067a8fa4e7fe20a8f91856a42b4f
Gabriel Lucca Marsola <mgabriellucca@gmail.com>
2025-11-02 18:37:12 -0300

    Adcionei uns comentários explicativos no código

--- Fundamentos/SocialNetworkGraph.java
+++ Fundamentos/SocialNetworkGraph.java
@@ -2,23 +2,33 @@ import java.util.*;
 
 public class SocialNetworkGraph {
 
+  // Usamos um HashMap para implementar a Lista de Adjacência.
+  // A Chave é o nome da pessoa (Vértice).
+  // O Valor é uma Lista de seus amigos (Vértices adjacences)
+
   private Map<String, List<String>> adjacencies = new HashMap<>();
   
-
+  // Adciona uma nova pessoa (vértice) ao grafo.
   public void addVertex(String person) {
     adjacencies.putIfAbsent(person, new ArrayList<>());
 
   }
   
+  // Adciona uma amizade (aresta) entre duas pessoas.
+  // Como é uma rede social, a amizade é mútua (não-direcionada).
   public void addEdge(String person1, String person2) {
+    // Garante que ambos existem no grafo.
     addVertex(person1);
     addVertex(person2);
 
+    // Adiciona person2 à lista de amigos de person1.
     adjacencies.get(person1).add(person2);
 
+    // Adiciona person1 à lista de amigos de person2.
     adjacencies.get(person2).add(person1);
   }
-
+  
+  // Retorna a lista de amigos de uma pessoa.
   public List<String> getFriends(String person) {
     return adjacencies.get(person);
   }
@@ -31,7 +41,7 @@ public class SocialNetworkGraph {
     network.addEdge("Bob", "David");
     network.addEdge("Carol", "David");
 
-
+    // Imprimindo as conexões
     System.out.println("Amigos da Alice: " + network.getFriends("Alice"));
     System.out.println("Amigos do Bob: " + network.getFriends("Bob"));
     System.out.println("Amigos do David " + network.getFriends("David"));
